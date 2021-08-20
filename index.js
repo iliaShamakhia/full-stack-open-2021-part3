f192f9d80022dd15e4cdfdc7f0182daaacbb47fd
iliaShamakhia <shamakhiailia@gmail.com>
2021-08-20 14:32:11 +0400

    full-stack

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express =require('express');
 const morgan =require('morgan');
 const cors=require('cors');
+const Person = require('./models/person');
 
 const app=express();
 app.use(express.static('build'));
@@ -13,58 +15,37 @@ morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body',{
   skip: function (req, res) { return req.method !=='POST'}
 }));
-let phonebook=[
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 app.get('/api/persons',(request,response)=>{
-    response.json(phonebook);
+  Person.find({}).then(persons => {
+    response.json(persons);
+  });
 });
 
-app.get('/api/persons/:id',(request,response)=>{
-  let id = Number(request.params.id);
-  let person =phonebook.find(el=>el.id===id);
-  if(person){
+app.get('/api/persons/:id',(request,response,next)=>{
+  Person.findById(request.params.id).then(person => {
+  if(person){  
     response.json(person);
   }else{
     response.status(404).end();
   }
+  }).catch(error=>next(error));
 });
 
-app.put('/api/persons/:id',(request,response)=>{
-  let id = Number(request.params.id);
+app.put('/api/persons/:id',(request,response,next)=>{
   let body=request.body;
-  let person =phonebook.find(el=>el.id===id);
-  let temp={
-    name:person.name,
+  let person={
+    name:body.name,
     number:body.number,
-    id:person.id
-  }
-  phonebook=phonebook.concat(temp);
-  response.json(temp);
+  };
+  Person.findByIdAndUpdate(request.params.id, person, { new: true,runValidators:false })
+    .then(updatedPerson => {
+      response.json(updatedPerson);
+    })
+    .catch(error => next(error))
 });
 
-app.post('/api/persons',(request,response)=>{
-
+app.post('/api/persons',(request,response,next)=>{
   const body = request.body;
 
   if (!body.name || !body.number) {
@@ -72,35 +53,41 @@ app.post('/api/persons',(request,response)=>{
       error: 'name and number required' 
     });
   }
-  let found=phonebook.find(el=>el.name===body.name);
-  if(found){
-    return response.status(400).json({ 
-      error: 'name must be unique' 
-    });
-  }
-
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: Math.floor(Math.random() * 999999)
-  }
-
-  phonebook = phonebook.concat(person);
-
-  response.json(person);
+  });
+  person.save().then(savedPerson => {
+    response.json(savedPerson);
+  }).catch(error=>next(error));
 });
 
-app.delete('/api/persons/:id',(request,response)=>{
-  let id = Number(request.params.id);
-  phonebook=phonebook.filter(el=>el.id!==id);
-  response.status(204).end();
+app.delete('/api/persons/:id',(request,response,next)=>{
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 });
 
 app.get('/info',(request,response)=>{
-    let persons=phonebook.length;
-    response.send(`<p>Phonebook has info for ${persons} people</p>
+  Person.countDocuments().then((count) =>{
+    response.send(`<p>Phonebook has info for ${count} people</p>
                    <p>${new Date()}</p>`);
+        });
 });
 
-const PORT=process.env.PORT || 3001;
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {    
+    return response.status(400).json({ error: error.message});
+  }
+  next(error);
+};
+app.use(errorHandler);
+
+const PORT=process.env.PORT;
 app.listen(PORT,()=>console.log(`listening at port ${PORT}`));
